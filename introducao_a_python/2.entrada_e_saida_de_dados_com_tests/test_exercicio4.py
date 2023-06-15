e5ad9ee1ae6fc7090c8987550eccd4f84cc7d7f8
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-06-15 18:30:46 -0300

    Exercicio quatro
    Teste desenvolvido não passando

--- introducao_a_python/2.entrada_e_saida_de_dados_com_tests/test_exercicio4.py
+++ introducao_a_python/2.entrada_e_saida_de_dados_com_tests/test_exercicio4.py
@@ -3,3 +3,7 @@ from exercicio4 import fizzbuzz
 
 def test_fizzbuzz_should_return_list_of_numbers():
     assert fizzbuzz(2) == [1, 2]
+
+
+def test_fizzbuzz_divisible_by_three_should_be_fizz():
+    assert fizzbuzz(3)[-1] == 'Fizz'
