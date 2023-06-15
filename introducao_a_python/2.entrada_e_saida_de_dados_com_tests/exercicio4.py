a8ff93a661edb780fd76e3847faffca3f99e807a
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-06-15 18:32:46 -0300

    Exercicio quatro
    Função criada e passadno no test

--- introducao_a_python/2.entrada_e_saida_de_dados_com_tests/exercicio4.py
+++ introducao_a_python/2.entrada_e_saida_de_dados_com_tests/exercicio4.py
@@ -1,5 +1,8 @@
 def fizzbuzz(n):
     numbers = []
     for number in range(1, n + 1):
-        numbers.append(number)
+        if number % 3 == 0:
+            numbers.append('Fizz')
+        else:
+            numbers.append(number)
     return numbers
