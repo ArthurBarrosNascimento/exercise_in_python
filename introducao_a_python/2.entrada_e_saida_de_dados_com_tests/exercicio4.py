0c3e1d4682346eba10ff10237c3d396d0098c3cb
ArthurBarrosNascimento <arthurbarros.nascimento12@gmail.com>
2023-06-15 17:16:45 -0300

    Exercicio em produção
    Este exercicio pede o uso do TDD para ser desenvolvido

---
+++ introducao_a_python/2.entrada_e_saida_de_dados_com_tests/exercicio4.py
@@ -0,0 +1,5 @@
+def fizzbuzz(n):
+    numbers = []
+    for number in range(1, n + 1):
+        numbers.append(number)
+    return numbers
---
+++ introducao_a_python/2.entrada_e_saida_de_dados_com_tests/test_exercicio4.py
@@ -0,0 +1,5 @@
+from exercicio4 import fizzbuzz
+
+
+def test_fizzbuzz_should_return_list_of_numbers():
+    assert fizzbuzz(2) == [1, 2]
